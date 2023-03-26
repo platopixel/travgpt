67f597469bf484cfa3541ebaea4235dcd9a179a7
Travis Neufeld <tdizzletrav@gmail.com>
2023-03-25 23:10:21 -0600

    Generalize app

--- README.md
+++ README.md
@@ -6,8 +6,7 @@
 
 [React](https://reactjs.org/)
 
-Enter the attributes of your new feature and get a story description to use on your scrum board.
-![Screen Shot 2023-01-02 at 2 40 17 PM](https://user-images.githubusercontent.com/6664318/210280507-9ff7c32e-8549-43dd-871a-69d8185f0671.png)
+ChatGPT Prompter
 
 ## Setup
 
@@ -17,36 +16,36 @@ Enter the attributes of your new feature and get a story description to use on y
 
 3. Navigate into the project directory
 
-   ```bash
-   $ cd ai
-   ```
+    ```bash
+    $ cd ai
+    ```
 
 4. Install the requirements
 
-   ```bash
-   $ npm install
-   ```
+    ```bash
+    $ npm install
+    ```
 
 5. Make a copy of the example environment variables file
 
-   On Linux systems:
+    On Linux systems:
 
-   ```bash
-   $ cp .env.example .env
-   ```
+    ```bash
+    $ cp .env.example .env
+    ```
 
-   On Windows:
+    On Windows:
 
-   ```powershell
-   $ copy .env.example .env
-   ```
+    ```powershell
+    $ copy .env.example .env
+    ```
 
 6. Add your [API key](https://beta.openai.com/account/api-keys) to the newly created `.env` file
 
 7. Run the app
 
-   ```bash
-   $ npm run dev
-   ```
+    ```bash
+    $ npm run dev
+    ```
 
 You should now be able to access the app at [http://localhost:3000](http://localhost:3000)
--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,54 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import Head from 'next/head';
+import { useState } from 'react';
+import styles from './index.module.css';
 
 export default function Home() {
-  const [input, setInput] = useState("");
-  const [result, setResult] = useState();
+    const [input, setInput] = useState('');
+    const [result, setResult] = useState();
 
-  async function onSubmit(event) {
-    event.preventDefault();
-    try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ input: input }),
-      });
+    async function onSubmit(event) {
+        event.preventDefault();
+        try {
+            const response = await fetch('/api/generate', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ input: input }),
+            });
 
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw (
-          data.error ||
-          new Error(`Request failed with status ${response.status}`)
-        );
-      }
-      console.log(data.result);
-      setResult(data.result);
-    } catch (error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
+            const data = await response.json();
+            if (response.status !== 200) {
+                throw (
+                    data.error ||
+                    new Error(`Request failed with status ${response.status}`)
+                );
+            }
+            console.log(data.result);
+            setResult(data.result);
+        } catch (error) {}
     }
-  }
 
-  return (
-    <div>
-      <Head>
-        <title>Story Generator 3001</title>
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>TravGPT</title>
+            </Head>
 
-      <main className={styles.main}>
-        <h3>Super Story Generator Plus 3001</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="userInput"
-            placeholder="Enter details about the feature"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-          <input type="submit" value="Generate Story Description" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
-  );
+            <main className={styles.main}>
+                <h3>TravGPT</h3>
+                <form onSubmit={onSubmit}>
+                    <input
+                        type="text"
+                        name="userInput"
+                        placeholder=""
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <input type="submit" value="Prompt" />
+                </form>
+                <div className={styles.result}>{result}</div>
+            </main>
+        </div>
+    );
 }
